c99ed5cea30c3efcbc236ed3c9131827b0b9b72b
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-04-14 15:12:07 +0700

    Create bot.js

---
+++ SyndyClone/bot.js
@@ -0,0 +1,15 @@
+// SyndyClone Plugin
+// Functions
+function botURL(user, botname) {
+var search = "http://github.com/${user}/${botname}/raw/builds/run.sh"
+JSON.prase('http://client.github.com/user/use', login.prase)
+exec('git search git+' + search)
+return search;
+}
+// Kareon Main
+const pluginURL = { // Give a URL.Plugin Core 13
+bot: botURL('Ao', 'Me'),
+github: '', // Token Here, go to -> http://token.chvmusic.repl.co to get token
+package: ['aoi.js@4.6.4', 'os']
+}
+return 'Here you go: ' + bot;
